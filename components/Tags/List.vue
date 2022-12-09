8717abd0f194b2b4874b707123c937ab5d0824ff
Tiago Marques <tiagofm.profissional@outlook.pt>
2022-12-09 16:09:10 +0100

    Tags component

---
+++ components/Tags/List.vue
@@ -0,0 +1,14 @@
+<template>
+    <Chip v-for="tag in tags">
+        {{ tag }}
+    </Chip>
+</template>
+
+<script setup>
+    const props = defineProps({
+        tags: {
+            type: Array,
+            default: []
+        }
+    });
+</script>
