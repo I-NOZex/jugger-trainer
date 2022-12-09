1a170164a73f54627f9f040e2998914791f8abcc
Tiago Marques <tiagofm.profissional@outlook.pt>
2022-12-09 16:07:31 +0100

    common components

---
+++ components/Chip.vue
@@ -0,0 +1,14 @@
+<template>
+    <button :class="`mr-1 text-xs inline-flex flex-shrink-0 flex- items-center font-bold leading-sm lowercase px-2 py-1 rounded border border-dark-secondaryText/10 ${ isActive ? 'bg-green-500' :'bg-white dark:bg-dark-primaryBg/10' }`">
+        <slot />
+    </button>
+</template>
+
+<script setup>
+const props = defineProps({
+    isActive: {
+        type: Boolean,
+        default: false
+    },
+})
+</script>
---
+++ components/RoundedBlock.vue
@@ -0,0 +1,18 @@
+<template>
+    <component :is="tag" :class="[!transparent && 'bg-white dark:bg-neutral-800', 'rounded-4-xl', 'p-4', 'shadow-md']">
+        <slot />
+    </component>
+</template>
+
+<script setup>
+const props = defineProps({
+    tag: {
+        type: String,
+        default: "div"
+    },
+    transparent: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
