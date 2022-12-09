bf399aefbd8259fa3492459868ccf1ceda5ae0c5
Tiago Marques <tiagofm.profissional@outlook.pt>
2022-12-09 17:51:51 +0100

    Listbox component

---
+++ components/Listbox/Option.vue
@@ -0,0 +1,36 @@
+<template>
+    <HeadlessListboxOption
+        as="div"
+        class="px-1 py-1"
+        v-slot="{ active, selected }"
+    >
+        <li
+            :class="[
+                active
+                    ? 'bg-neutral-900/50 text-dark-primaryText'
+                    : ' text-dark-secondaryText',
+                'group flex w-full items-center rounded-lg relative py-4 pl-10 pr-4 text-sm'
+            ]"
+        >
+            <span
+                :class="[
+                    selected ? 'font-medium' : 'font-normal',
+                    'block truncate'
+                ]"
+            >
+                <slot />
+            </span>
+
+            <span
+                v-if="selected"
+                class="absolute inset-y-0 left-0 flex items-center pl-3 text-primary"
+            >
+                <Icon
+                    class="h-5 w-5"
+                    name="material-symbols:check-small"
+                    aria-hidden="true"
+                />
+            </span>
+        </li>
+    </HeadlessListboxOption>
+</template>
---
+++ components/Listbox/Options.vue
@@ -0,0 +1,7 @@
+<template>
+    <HeadlessListboxOptions
+        class="absolute left-0 mt-2 p-4 w-max min-w-full origin-top-right rounded-lg bg-white dark:bg-neutral-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+    >
+        <slot />
+    </HeadlessListboxOptions>
+</template>
