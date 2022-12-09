a117f12d72632736a2795804c3836c4136c411c3
Tiago Marques <tiagofm.profissional@outlook.pt>
2022-12-09 18:13:00 +0100

    use listbox component

--- components/Filter/Difficulty.vue
+++ components/Filter/Difficulty.vue
@@ -1,24 +1,23 @@
 <template>
-    <HLDropdown prefix="yolo" label="Difficulty">
+    <Listbox v-model="selected" :label="selected?.name || 'Dificulty'">
         <template #prefix>
             <Icon aria-hidden="true" name="icon-park-outline:muscle" />
         </template>
 
-        <HLDropdownMenu>
-            <HLDropdownItem v-for="item in items" :label="item.label" :meta="item.meta">
-                <Icon :name="item.icon" />
-            </HLDropdownItem>
-        </HLDropdownMenu>
-    </HLDropdown>
+        <ListboxOptions>
+            <ListboxOption v-for="item in items" :key="item.id" :value="item">
+                {{item.name}}
+            </ListboxOption>
+        </ListboxOptions>
+    </Listbox>
 </template>
-
 
 <script setup>
     const items = [
-        {icon: 'icon-park-outline:muscle', label: "Nulla velit mollit labore"},
-        {icon: 'icon-park-outline:muscle', label: "Nulla velit mollit labore"},
-        {icon: 'icon-park-outline:muscle', label: "Nulla velit mollit labore", meta: "Labore nulla ex"},
-        {icon: 'icon-park-outline:muscle', label: "Nulla velit mollit labore"},
-        {icon: 'icon-park-outline:muscle', label: "Nulla velit mollit labore"},
-    ]
+        { id: 1, name: 'Novice' },
+        { id: 2, name: 'Advanced' },
+        { id: 3, name: 'Expert' }
+    ];
+
+    const selected = ref(null);
 </script>
